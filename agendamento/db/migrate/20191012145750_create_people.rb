1d420b55da84348f58754466f9e0596ddad3473c
neilprado <neiljohn17@gmail.com>
2019-10-12 11:17:34 -0500

    agendamento

---
+++ agendamento/db/migrate/20191012145750_create_people.rb
@@ -0,0 +1,10 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :nome
+      t.string :abreviacao
+
+      t.timestamps
+    end
+  end
+end
