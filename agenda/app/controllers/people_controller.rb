8820c19443d750219928a36cbc2ce1edbb81f7a6
neilprado <neiljohn17@gmail.com>
2019-10-31 17:06:03 -0300

    Finalizando projeto

--- agenda/app/controllers/people_controller.rb
+++ agenda/app/controllers/people_controller.rb
@@ -54,6 +54,9 @@ class PeopleController < ApplicationController
   # DELETE /people/1
   # DELETE /people/1.json
   def destroy
+    @person.contacts.each do |contact|
+      contact.destroy
+    end
     @person.destroy
     respond_to do |format|
       format.html { redirect_to people_url, notice: 'Person was successfully destroyed.' }
